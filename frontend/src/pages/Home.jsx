246ac0484afb72a72ebdd52b839f9e4f7e0b0abd
codebyamulya <codebyamulya@gmail.com>
2026-07-22 22:41:48 +0530

    Add Home page changes

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,30 @@
+import "../App.css";
+
+import Navbar from "../components/Navbar/Navbar";
+import Hero from "../components/Hero/Hero";
+import HowItWorks from "../components/HowItWorks/HowItWorks";
+import Features from "../components/Features/Features";
+import CTA from "../components/CTA/CTA";
+import Footer from "../components/Footer/Footer";
+
 function Home() {
-  return <h1>Home Page</h1>;
+  return (
+    <div className="app">
+      <div className="app__bg-glow" />
+
+      <Navbar />
+
+      <Hero />
+
+      <HowItWorks />
+
+      <Features />
+
+      <CTA />
+
+      <Footer />
+    </div>
+  );
 }
 
 export default Home;
